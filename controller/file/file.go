1cd903c07907e2026ce937ef02e0f7f919155879
fuyunfan <yunfan.fu@biomind.ai>
2021-12-23 15:26:40 +0800

    更换成测试用的

--- controller/file/file.go
+++ controller/file/file.go
@@ -42,12 +42,13 @@ func Files(ctx *gin.Context) {
 		FileName: paths[len(paths)-1],
 	}
 
-	fileMetadata, err := getFileMetadataFromFile(ctx, p.Bucket, p.FileName)
-	if err != nil {
-		ctx.Writer.WriteHeader(http.StatusNotFound)
-		output.Json(ctx, file_server.ErrorGetFileMetadata, err.Error())
-		return
-	}
+	//todo 测试用的
+	//fileMetadata, err := getFileMetadataFromFile(ctx, p.Bucket, p.FileName)
+	//if err != nil {
+	//	ctx.Writer.WriteHeader(http.StatusNotFound)
+	//	output.Json(ctx, file_server.ErrorGetFileMetadata, err.Error())
+	//	return
+	//}
 
 	if ctx.Request.Header.Get("Content-Type") == "" {
 		ctx.Writer.Header().Add("Content-type", "application/octet-stream")
@@ -55,10 +56,11 @@ func Files(ctx *gin.Context) {
 		ctx.Writer.Header().Add("Content-Type", ctx.Request.Header.Get("Content-Type"))
 	}
 
-	ctx.Writer.Header().Add("e_tage", fileMetadata.ETage)
-	ctx.Writer.Header().Add("header_custom", fileMetadata.Header)
-	ctx.Writer.Header().Add("file_size", fmt.Sprintf("%d", fileMetadata.FileSize))
-	ctx.Writer.Header().Add("file_extension", fileMetadata.FileExtension)
+	//todo 测试用的
+	//ctx.Writer.Header().Add("e_tage", fileMetadata.ETage)
+	//ctx.Writer.Header().Add("header_custom", fileMetadata.Header)
+	//ctx.Writer.Header().Add("file_size", fmt.Sprintf("%d", fileMetadata.FileSize))
+	//ctx.Writer.Header().Add("file_extension", fileMetadata.FileExtension)
 
 	filePath := path.Join(utils.GetCurrentAbPath(), "data", p.Bucket, p.FileName)
 	if exists(filePath) {
